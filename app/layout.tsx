8ecd3d31cb40ec5da3f2a24b31b430a499c8f1cc
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2025-03-10 16:39:47 +0100

    hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,21 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <head>
         <StructuredData />
+        {/* Hotjar Tracking Code for Site 5330056 (name missing) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:5330056,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.variable} font-sans`} suppressHydrationWarning>
         <Header />
